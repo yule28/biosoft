2a58f52ec01f8e958fff75a6926dc8b6aa04261f
Yule <yuleuzca@gmail.com>
2016-06-22 15:21:55 -0400

    Modificación de la barra de navegación
    Se agregaron nuevos items al menú y se agruparon los item: consultoría,
    capacitación y desarrollo en uno solo.

--- app/views/parciales/menu.blade.php
+++ app/views/parciales/menu.blade.php
@@ -17,10 +17,16 @@
 		<div class="collapse navbar-collapse" id="bs-menu">
 			<ul class="ph-line-nav nav navbar-nav">
 				<li class="{{Request::path() == '/' ? 'active' : '';}}">{{ HTML::link('/', 'INICIO'); }}</li>
-				<li class="{{Request::path() == 'consultoria' ? 'active' : '';}}">{{ HTML::link('consultoria', 'CONSULTORIA'); }}</li>
-				<li class="{{Request::path() == 'capacitacion' ? 'active' : '';}}">{{ HTML::link('capacitacion', 'CAPACITACIÓN'); }}</li>
-				<li class="{{Request::path() == 'desarrollo' ? 'active' : '';}}">{{ HTML::link('desarrollo', 'DESARROLLO'); }}</li>
-				<li class="{{Request::path() == 'equipo' ? 'active' : '';}}">{{ HTML::link('equipo', 'EQUIPO'); }}</li>
+				<li class="{{Request::path() == 'equipo' ? 'active' : '';}}">{{ HTML::link('equipo', 'EMPRESA'); }}</li>
+				<li class="dropdown">{{ HTML::link('#', 'SERVICIOS', $attributes = array('class' => 'dropdown-toggle', 'data-toggle' => "dropdown", 'role' => "button", 'aria-haspopup' => "true", 'aria-expanded' => "false")); }}
+				<ul class="dropdown-menu">
+					<li class="{{Request::path() == 'consultoria' ? 'active' : '';}}">{{ HTML::link('consultoria', 'CONSULTORIA'); }}</li>
+					<li class="{{Request::path() == 'capacitacion' ? 'active' : '';}}">{{ HTML::link('capacitacion', 'CAPACITACIÓN'); }}</li>
+					<li class="{{Request::path() == 'desarrollo' ? 'active' : '';}}">{{ HTML::link('desarrollo', 'DESARROLLO'); }}</li>
+				</ul>
+				</li>
+				
+				<li class="{{Request::path() == 'portafolio' ? 'active' : '';}}">{{ HTML::link('portafolio', 'PORTAFOLIO'); }}</li>
 				<li class="{{Request::path() == 'contacto' ? 'active' : '';}}">{{ HTML::link('contacto', 'CONTACTO'); }}</li>
 				<div class="effect"></div>
 			</ul> <!-- /.navbar-nav -->
