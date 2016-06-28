79d58a24b1040bddbfd60f8483f855e5e1c581f6
seven <jheancg@gmail.com>
2016-06-28 17:17:26 -0400

    Se termino la seccion de clientes en el home y la de porque nosotros

--- app/views/estaticas/inicio.blade.php
+++ app/views/estaticas/inicio.blade.php
@@ -13,12 +13,12 @@
 				    <div class="text-center text-indicadores">Más de 9 años de experiencia</div>
 				</div>
 				<div class="col-md-3">
-                    {{ HTML::image('assets/images/inicio/cliente.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
-				    <div class="text-center text-indicadores">Más de 50 clientes felices</div>
+                    {{ HTML::image('assets/images/inicio/aplicacion.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
+				    <div class="text-center text-indicadores">Más de 17 aplicaciones desarrolladas</div>
 				</div>
 				<div class="col-md-3">
                     {{ HTML::image('assets/images/inicio/proyectos.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
-				    <div class="text-center text-indicadores">100 proyectos desarrollados</div>
+				    <div class="text-center text-indicadores">21 proyectos desarrollados</div>
 				</div>
 				<div class="col-md-3">
                     {{ HTML::image('assets/images/inicio/grupo.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
@@ -33,49 +33,43 @@
 		<div class="container">
 		<div class="titulo text-center">Por que nosotros</div>
 			<div class="row">
-                <div class="col-md-2">
-                    {{ HTML::image('assets/images/inicio/check.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
+                <div class="col-md-1">
+                    {{ HTML::image('/assets/images/inicio/consultoria.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
                 </div>
-				<div class="col-md-4">
-					<h4>Tomamos las ideas y las transformamos en software de calidad </h4>
-					<p class="text-justify">Creamos aplicaciones y sistemas
-					acorde a las necesidades de cada empresa. Métodos ágiles y
-					disciplinados que se traducen en desarrollo de software rápido,
-					ordenado y eficaz.</p>
+				<div class="col-md-3">
+					<h4>CONSULTORÍA </h4>
+					<ul class="check-list">
+					    <li>Alineamos las TIC al negocio</li>
+					    <li>Tenemos alta experiencia profesional y estudios de 4to y 5to nivel</li>
+					    <li>Asesoramos a grandes empresas nacionales e internacionales</li>
+					    <li>Nos enfocamos en tecnologías de punta: BPM, SOA, Arquitecturas empresariales</li>
+					</ul>
 				</div>
 
-				                <div class="col-md-2">
-                    {{ HTML::image('assets/images/inicio/check.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
+				<div class="col-md-1">
+                    {{ HTML::image('assets/images/inicio/capacitacion.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
                 </div>
-				<div class="col-md-4">
-					<h4>Tomamos las ideas y las transformamos en software de calidad </h4>
-					<p class="text-justify">Creamos aplicaciones y sistemas
-					acorde a las necesidades de cada empresa. Métodos ágiles y
-					disciplinados que se traducen en desarrollo de software rápido,
-					ordenado y eficaz.</p>
+				<div class="col-md-3">
+					<h4>CAPACITACIÓN</h4>
+					<ul class="check-list">
+					    <li>Contamos con profesores de 4to y 5to nivel académico</li>
+					    <li>Tenemos experiencia de 20 años en formación de profesionales en Ingeniería de Software</li>
+					    <li>Orientamos la capacitación a la práctica profesional</li>
+					    <li>Capacitamos para que seas competitivo en tecnologías de información</li>
+					</ul>
 				</div>
-			</div><br><br>
-			<div class="row">
-                <div class="col-md-2">
-                    {{ HTML::image('assets/images/inicio/check.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
+                <div class="col-md-1">
+                    {{ HTML::image('assets/images/inicio/desarrollo.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
                 </div>
-				<div class="col-md-4">
-					<h4>Tomamos las ideas y las transformamos en software de calidad </h4>
-					<p class="text-justify">Creamos aplicaciones y sistemas
-					acorde a las necesidades de cada empresa. Métodos ágiles y
-					disciplinados que se traducen en desarrollo de software rápido,
-					ordenado y eficaz.</p>
-				</div>
-
-				                <div class="col-md-2">
-                    {{ HTML::image('assets/images/inicio/check.png', $alt='15 años de experiencia', $attributes = array('title' => '9 años de experiencia', 'class' => 'img-responsive')) }} 
-                </div>
-				<div class="col-md-4">
-					<h4>Tomamos las ideas y las transformamos en software de calidad </h4>
-					<p class="text-justify">Creamos aplicaciones y sistemas
-					acorde a las necesidades de cada empresa. Métodos ágiles y
-					disciplinados que se traducen en desarrollo de software rápido,
-					ordenado y eficaz.</p>
+				<div class="col-md-3">
+					<h4>DESARROLLO DE SOFTWARE</h4>
+					<ul class="check-list">
+					    <li>Tenemos experiencia de 20 años en desarrollo de software</li>
+					    <li>Nos especializamos en aplicaciones web para soportar procesos de negocio</li>
+					    <li>Usamos métodos ágiles, herramientas CASE y lenguaje de modelado UML</li>
+					    <li>Desarrollamos software guiado por pruebas</li>
+					    <li>Programamos en Ruby on Rails</li>
+					</ul>
 				</div>
 			</div>
 		</div>
@@ -87,24 +81,66 @@
 			</div>
 			<div class="row">
 				<div class="col-md-12">
-                <section class="regular slider">
+                <section class="regular ">
                     <div>
-                        <img src="http://placehold.it/350x300?text=1">
+                        <img src="assets/images/clientes/cencoex.jpg" alt="">
                     </div>
                     <div>
-                        <img src="http://placehold.it/350x300?text=2">
+                        <img src="assets/images/clientes/bod.jpg">
                     </div>
                     <div>
-                        <img src="http://placehold.it/350x300?text=3">
+                        <img src="assets/images/clientes/univ-lisandro.jpg">
                     </div>
                     <div>
-                        <img src="http://placehold.it/350x300?text=4">
+                        <img src="assets/images/clientes/enelven.jpg">
                     </div>
                     <div>
-                        <img src="http://placehold.it/350x300?text=5">
+                        <img src="assets/images/clientes/ceisoft.jpg">
                     </div>
                     <div>
-                        <img src="http://placehold.it/350x300?text=6">
+                        <img src="assets/images/clientes/bcv.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/ula.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/gasprom.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/caf.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/smurfit.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/pdvsa.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/min-ambiente.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/seniat.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/cvg.jpg">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/ena-for.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/farmatodo.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/mad-orinoco.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/intt.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/va-isid.jpg" alt="">
+                    </div>
+                    <div>
+                        <img src="assets/images/clientes/guardianes.jpg" alt="">
                     </div>
                 </section>
 				</div>
