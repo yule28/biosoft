fc8b4f8cc25c3b723c35629d35fa996f28c3d4be
Yule <yuleuzca@gmail.com>
2016-07-15 13:08:54 -0400

    modificación de estilos nuevo menú

--- app/views/parciales/menu-nuevo.blade.php
+++ app/views/parciales/menu-nuevo.blade.php
@@ -1,8 +1,40 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container-fluid">
-    <div class= "redes">
-      <div class="telefonos">
-        
+    <div class= "top-cabecera">
+      <div class="navbar-header">
+        <div class="logo-biosoft">
+          {{ HTML::image('assets/images/layout/logo_headernew.png',
+                    $alt="Biosof c.a", $attributes = array('width' => 212, 'height' => 80,
+                    'class' => 'img-responsive')) }}
+        </div>
+          
+      </div> <!-- ./navbar-header -->
+      <div class= "redes">
+       
+        <div class="correo">
+            <!--correo-->
+          <a href="contacto"> {{ HTML::image('assets/images/contacto/email.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
+          </a>
+        </div>
+        <div class="instagram">
+        <!--instagram-->
+          <a href="https://www.instagram.com/biosoftca/" target="_blank"> {{ HTML::image('assets/images/contacto/link.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
+          </a>
+        </div>
+
+        <div class="twitter">
+        <!--twitter-->
+          <a href="https://twitter.com/biosoftca" target="_blank"> {{ HTML::image('assets/images/contacto/twitter.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
+          </a>
+        </div>
+        <div class="facebook">
+        <!--facebook-->
+          <a href="https://www.facebook.com/Biosoftca-1622725348051680/" target="_blank"> {{ HTML::image('assets/images/contacto/face.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
+          </a>
+        </div>
+      
+      </div>
+     <div class="telefonos">
         <div class="telelocal">
           <p>Telf: (+58) 274-7892669</p>
         </div>
@@ -10,44 +42,11 @@
           <p>atencion@biosoftca.com</p>
         </div>
       </div>
-      <div class="correo">
-          <!--correo-->
-        <a href="contacto"> {{ HTML::image('assets/images/contacto/email.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
-        </a>
-      </div>
-      <div class="instagram">
-      <!--instagram-->
-        <a href="https://www.instagram.com/biosoftca/" target="_blank"> {{ HTML::image('assets/images/contacto/link.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
-        </a>
-      </div>
+    </div> 
 
-      <div class="twitter">
-      <!--twitter-->
-        <a href="https://twitter.com/biosoftca" target="_blank"> {{ HTML::image('assets/images/contacto/twitter.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
-        </a>
-      </div>
-      <div class="facebook">
-      <!--facebook-->
-        <a href="https://www.facebook.com/Biosoftca-1622725348051680/" target="_blank"> {{ HTML::image('assets/images/contacto/face.png', $alt="imagen 1", $attributes = array('width' => 30, 'height' => 30, 'class' => 'img-responsive')) }}
-        </a>
-      </div>
-      
-    </div>
-    
-    <div class="navbar-header">
-      <div class="logo-biosoft">
-        {{ HTML::image('assets/images/layout/logo_headernew.png',
-                  $alt="Biosof c.a", $attributes = array('width' => 212, 'height' => 80,
-                  'class' => 'img-responsive')) }}
-      </div>
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-menu">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-    </div> <!-- ./navbar-header -->
-    
+  </div> <!-- /.container-fluid -->
+
+  <div class="container-fluid">
     <div class="collapse navbar-collapse" id="bs-menu">
       <ul class="ph-line-nav nav navbar-nav">
         <li class="{{Request::path() == '/' ? 'active' : '';}}">{{ HTML::link('/', 'INICIO'); }}</li>
@@ -62,7 +61,6 @@
         <li class="{{Request::path() == 'contacto' ? 'active' : '';}}">{{ HTML::link('contacto', 'CONTACTO'); }}</li>
         <div class="effect"></div>
       </ul> <!-- /.navbar-nav -->
-    </div>  
-
-  </div> <!-- /.container-fluid -->
+    </div> 
+  </div>
 </nav><!-- /.nav -->
